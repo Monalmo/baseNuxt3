ee3b30143238c75c21ac9e65210f04983b4e04a4
Cristian Hadad Guerra <cristian.hadad.g@gmail.com>
2022-12-14 16:13:00 -0300

    primer commit

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+div
+	h1 holooooo
+	.comentario este es el comen tario
+		| {{algo}}		
+		.algoMas algoMas
+			|	{{algo2}}
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const algo = ref('algooooooo')
+const algo2 = ref({
+	hh: 'd',
+	gg: '3'
+})
+</script>
+
+<style lang="sass" scoped>
+
+h1
+	color: red
+
+
+.comentario
+	color: blue
+	.algoMas
+		color: green
+</style>
